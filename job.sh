1aec3b4edaa8d0b81caf18c69225adeca6afcb20
Lukasz Miroslaw <lukmiros@gmail.com>
2020-01-15 16:29:09 -0800

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,17 @@
+echo Hostfile:
+echo `cat $PBS_NODEFILE`
+echo
+date
+sudo beeond start -n $(readlink -f $SLURM_JOB_NODELIST) -d /mnt/resource/beeond -c /beeond
+date
+echo Finished beeond
+echo `df -h /beeond`
+echo
+sleep 5
+
+mpirun hostname
+ 
+# Let's stop Beeond now :
+date
+time sudo beeond stop -n $(readlink -f $SLURM_JOB_NODELIST) -L -d
+date
