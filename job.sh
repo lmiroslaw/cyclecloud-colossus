038980b4a881a6049c7a95b31c4b6ccaf5dc2254
Lukasz Miroslaw <lukmiros@gmail.com>
2020-01-15 16:57:24 -0800

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,3 +1,14 @@
+SBATCH --job-name=imb
+#SBATCH --output=imb_hc.txt
+#SBATCH --account=tsp
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=44
+#SBATCH --time=60:00
+#SBATCH --partition=HC44
+module load mpi/impi_2018.4.274
+
 echo Hostfile:
 echo `cat $SLURM_JOB_NODELIST`
 echo
@@ -9,7 +20,7 @@ echo `df -h /beeond`
 echo
 sleep 5
 
-mpirun hostname
+mpirun -np 2 -ppn 44 -genv I_MPI_FABRICS=shm:ofa -genv I_MPI_FALLBACK_DEVICE=0 IMB-MPI1 hostname
  
 # Let's stop Beeond now :
 date
